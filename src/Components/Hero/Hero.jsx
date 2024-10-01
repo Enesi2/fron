5ea68bb24cb5b95fb63491c7998bfb7bfe393804
Enesi Emmanuel Omeiza <emmymy2@gmail.com>
2024-10-01 06:28:40 -0700

    fron

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import './Hero.css';
+
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-left">
+            <h2>NEW ARRIVALS ONLY</h2>
+            <div>
+                <div className="hand-hand-icon">
+                    <p>new</p>
+                  
+                </div>
+            </div>
+        </div>
+        <div className="hero-right">
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
